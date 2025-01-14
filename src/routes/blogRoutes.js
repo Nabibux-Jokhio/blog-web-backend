3c8f0102cea8a8dd0e9c9d59c091c0e7d43abeb1
Nabibux-Jokhio <jokhionb12@gmail.com>
2025-01-14 22:43:54 +0500

    delete and update route

--- src/routes/blogRoutes.js
+++ src/routes/blogRoutes.js
@@ -24,6 +24,18 @@ blogRoute.get("/blog/add", async (req, res) => {
     }
 })
 
+blogRoute.put("/blog/update/:id",async(req , res)=>{
+
+     try {
+        const {id} = req.params;
+        const data = req.body;
+        const response = await Blog.findByIdAndUpdate(id,data)
+        res.status(200).send({ status: 200, message: enums.UPDATE_MSG, data: response })
+    } catch (error) {
+        res.status(400).send({ status: 400, message: enums.ERROR_CONNECTION })
+    }
+})
+
 blogRoute.delete("/blog/delete/:id",async(req , res)=>{
 
      try {
